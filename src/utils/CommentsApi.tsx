0e8b514436be5103a7aca24386470bc029f699ae
Mahfod <mahfod.dev@gmail.com>
2023-02-23 15:32:40 +0100

    fixed bug

--- src/utils/CommentsApi.tsx
+++ src/utils/CommentsApi.tsx
@@ -3,5 +3,5 @@ import { CommentsData } from "../interfaces/CommentsData";
 export const CommentsApi = async (): Promise<CommentsData[]> => {
   const response = await fetch("https://jsonplaceholder.typicode.com/comments");
   const data = await response.json();
-  return data;
+  return data.slice(0, 10);
 };
